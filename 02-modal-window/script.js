ca0828156c67cef5e1a4f9197be8fc08629d46d8
MostafaAE <mostafa.aymanae@gmail.com>
2023-02-13 23:27:48 +0200

    Implement show modal

--- 02-modal-window/script.js
+++ 02-modal-window/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
