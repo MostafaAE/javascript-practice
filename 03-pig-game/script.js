955ce2ec97d9d6b9cc1a2097783e86c310c346fb
MostafaAE <mostafa.aymanae@gmail.com>
2023-02-15 01:36:13 +0200

    Implement reset game

--- 03-pig-game/script.js
+++ 03-pig-game/script.js
@@ -9,17 +9,33 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
 const diceEl = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
 const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 
 // STARTING GAME CONDITIONS
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+  currentPlayer = 0;
+  currentScore = 0;
+  score = [0, 0];
+  playing = true;
 
-// ROLL DICE LOGIC
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+
+  diceEl.classList.add('hidden');
+};
+init();
 
 // Generate a random number between 1 : 6
 const generateNumber = () => Math.trunc(Math.random() * 6) + 1;
@@ -89,3 +105,4 @@ const holdScore = function () {
 
 btnHold.addEventListener('click', holdScore);
 btnRoll.addEventListener('click', rollDice);
+btnNew.addEventListener('click', init);
