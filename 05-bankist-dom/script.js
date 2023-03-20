f7cfff6d893d145b8f22a55e4f9de5d3e61870e7
MostafaAE <mostafa.aymanae@gmail.com>
2023-03-21 00:59:46 +0200

    Implement menu fade animation

--- 05-bankist-dom/script.js
+++ 05-bankist-dom/script.js
@@ -9,6 +9,7 @@ const section1 = document.querySelector('#section--1');
 const tabbedContainer = document.querySelector('.operations__tab-container');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 ///////////////////////////////////////
 // Modal window
 const openModal = function (e) {
@@ -96,3 +97,21 @@ tabbedContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${tabNum}`)
     .classList.add('operations__content--active');
 });
+
+///////////////////////////////////////
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
