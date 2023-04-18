fcf5b61c189da4dac8aa66d86e81ed91c33887c7
MostafaAE <mostafa.aymanae@gmail.com>
2023-04-18 04:40:16 +0200

    Load recipe from API

--- 07-forkify/src/js/controller.js
+++ 07-forkify/src/js/controller.js
@@ -11,3 +11,34 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+console.log('TEST TEST');
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcd09'
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(data.message);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      ingredients: recipe.ingredients,
+      cookingTime: recipe.cooking_time,
+      servings: recipe.servings,
+      image: recipe.image_url,
+      sourceUrl: recipe.source_url,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
