6a3553f29bd45c9c7d88ffb96e256d40f3943088
MostafaAE <mostafa.aymanae@gmail.com>
2023-04-19 02:09:45 +0200

    Implement success message

--- 07-forkify/src/js/views/recipeView.js
+++ 07-forkify/src/js/views/recipeView.js
@@ -5,7 +5,7 @@ class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
   #errorMessage = 'We could not find that recipe. Please try another one!';
-
+  #message = 'Success!';
   render(data) {
     this.#data = data;
     const markup = this.#generateMarkup();
@@ -141,6 +141,21 @@ class RecipeView {
         <div>
           <svg>
             <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this.#message) {
+    const markup = `
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
           </svg>
         </div>
         <p>${message}</p>
