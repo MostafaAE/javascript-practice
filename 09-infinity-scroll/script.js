ebac8f04ec92bd77e979dd49bf5ee32c7f7732a1
MostafaAE <mostafa.aymanae@gmail.com>
2023-05-05 01:41:53 +0300

    Implement infnite scroll functionality

--- 09-infinity-scroll/script.js
+++ 09-infinity-scroll/script.js
@@ -3,21 +3,54 @@ const loader = document.getElementById('loader');
 
 let photosArray = [];
 
+let ready = false;
+let imgsLoaded = 0;
+let totalImgs = 0;
 // Unsplace API
-const count = 10;
+const count = 30;
 // Normally,  don't store API Keys like this, but an exception made here because it is free, and the data is publicly available!
 const apiKey = 'TLWYYX7_18nvkJ2XHQwgHOglGOOsTouAJnVejyY_L8Q';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&content_filter=high&count=${count}&query=universe`;
 
+function setAttributes(element, attributes) {
+  for (const key in attributes) element.setAttribute(key, attributes[key]);
+}
+
+function imgLoaded() {
+  imgsLoaded++;
+
+  if (imgsLoaded === totalImgs) {
+    ready = true;
+  }
+}
+
 // Create elements for links & photos, Add to the DOM
-async function displayPhotos() {
+function displayPhotos() {
+  totalImgs = photosArray.length;
+  imgsLoaded = 0;
   photosArray.forEach(photo => {
     const markup = `
       <a href="${photo.links.html}" target="_blank">
         <img src="${photo.urls.regular}" alt="${photo.alt_description}" title="${photo.alt_description}">
       </a>
       `;
-    imgContainer.insertAdjacentHTML('beforeend', markup);
+
+    const item = document.createElement('a');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+    const img = document.createElement('img');
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    img.addEventListener('load', imgLoaded);
+
+    item.appendChild(img);
+    imgContainer.appendChild(item);
   });
 }
 
@@ -27,11 +60,21 @@ async function getPhotos() {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
     displayPhotos();
-    console.log(photosArray);
   } catch (error) {
     console.log(error);
   }
 }
+
+// Check to see if scrolling near the bottom of page, load more photos
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // On load
 getPhotos();
