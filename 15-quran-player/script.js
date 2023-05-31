79ad2ff51c1fe3fc79c07c043d15c5a8a0a5966e
MostafaAE <mostafa.aymanae@gmail.com>
2023-05-31 23:43:30 +0300

    Implement load previous & next audio functionality

--- 15-quran-player/script.js
+++ 15-quran-player/script.js
@@ -1,4 +1,6 @@
 const audioPlayer = document.querySelector('.audio');
+const audioTitle = document.querySelector('.title');
+const audioArtist = document.querySelector('.artist');
 const playBtn = document.querySelector('.main-button');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
@@ -8,6 +10,31 @@ const audioCurTime = document.querySelector('.current-time');
 const audioDuration = document.querySelector('.duration');
 
 let audioIsPlaying = false;
+let curAudioIdx = 0;
+
+class Audio {
+  constructor(fileName, displayName, artist) {
+    this.fileName = fileName;
+    this.displayName = displayName;
+    this.artist = artist;
+  }
+}
+
+const audios = [
+  new Audio('001', 'ماتيسر من سورة البقرة', 'عبد الرحمن مسعد'),
+  new Audio('002', 'ماتيسر من سورة آل عمران', 'عبد الرحمن مسعد'),
+  new Audio('003', 'ماتيسر من سورة الأعراف', 'عبد الرحمن مسعد'),
+  new Audio('004', 'ماتيسر من سورة الرعد 1', 'عبد الرحمن مسعد'),
+  new Audio('005', 'ماتيسر من سورة الرعد 2', 'عبد الرحمن مسعد'),
+  new Audio('006', 'ماتيسر من سورة إبراهيم', 'عبد الرحمن مسعد'),
+  new Audio('007', 'ماتيسر من سورة النحل 1', 'عبد الرحمن مسعد'),
+  new Audio('008', 'ماتيسر من سورة النحل 2', 'عبد الرحمن مسعد'),
+  new Audio('009', 'ماتيسر من سورة مريم 1', 'عبد الرحمن مسعد'),
+  new Audio('010', 'ماتيسر من سورة مريم 2', 'عبد الرحمن مسعد'),
+  new Audio('011', 'ماتيسر من سورة النور', 'عبد الرحمن مسعد'),
+  new Audio('012', 'ماتيسر من سورة العنكبوت', 'عبد الرحمن مسعد'),
+  new Audio('013', 'ماتيسر من سورة الروم', 'عبد الرحمن مسعد'),
+];
 
 const playAudio = function () {
   audioPlayer.play();
@@ -23,7 +50,32 @@ const pauseAudio = function () {
   audioIsPlaying = false;
 };
 
+const loadAudio = function (idx) {
+  if (idx < 0 || idx >= audios.length) return;
+  const audio = audios[idx];
+  audioTitle.textContent = audio.displayName;
+  audioArtist.textContent = audio.artist;
+  audioPlayer.src = `quran/${audio.fileName}.mp3`;
+};
+
+const nextAudio = function () {
+  curAudioIdx = (curAudioIdx + 1 + audios.length) % audios.length;
+  loadAudio(curAudioIdx);
+  playAudio();
+};
+const prevAudio = function () {
+  curAudioIdx = (curAudioIdx - 1 + audios.length) % audios.length;
+  loadAudio(curAudioIdx);
+  playAudio();
+};
+
 // Event Listeners
 playBtn.addEventListener('click', () => {
   audioIsPlaying ? pauseAudio() : playAudio();
 });
+
+prevBtn.addEventListener('click', prevAudio);
+
+nextBtn.addEventListener('click', nextAudio);
+
+loadAudio(0);
