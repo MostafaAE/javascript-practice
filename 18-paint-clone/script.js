cac4dddf5d8693a8f6c8d398740837d6192b4d8d
MostafaAE <mostafa.aymanae@gmail.com>
2023-06-11 23:15:33 +0300

    Implement save drawn image functionality

--- 18-paint-clone/script.js
+++ 18-paint-clone/script.js
@@ -192,12 +192,13 @@ clearStorageBtn.addEventListener('click', () => {
 });
 
 // // Download Image
-// downloadBtn.addEventListener('click', () => {
-
-//   // Active Tool
-//   activeToolEl.textContent = 'Image File Saved';
-//   setTimeout(switchToBrush, 1500);
-// });
+downloadBtn.addEventListener('click', () => {
+  downloadBtn.href = canvas.toDataURL('image/jpeg', 1);
+  downloadBtn.download = 'paint-example.jpeg';
+  // Active Tool
+  activeToolEl.textContent = 'Image File Saved';
+  setTimeout(switchToBrush, 1500);
+});
 
 // Event Listener
 brushIcon.addEventListener('click', switchToBrush);
