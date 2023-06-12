fd83dbeb1c092d3385052d6981b54144f3fea4ae
MostafaAE <mostafa.aymanae@gmail.com>
2023-06-12 19:23:55 +0300

    Render score on canvas

--- 19-pong/script.js
+++ 19-pong/script.js
@@ -21,6 +21,11 @@ let ballX = 250;
 let ballY = 350;
 const ballRadius = 5;
 
+// Score
+let playerScore = 0;
+let computerScore = 0;
+const winningScore = 7;
+
 function renderCanvas() {
   // Canvas background
   context.fillStyle = 'black';
@@ -48,6 +53,11 @@ function renderCanvas() {
   context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
   context.fillStyle = 'white';
   context.fill();
+
+  // Score
+  context.font = '32px Courier New';
+  context.fillText(playerScore, 20, canvas.height / 2 + 50);
+  context.fillText(computerScore, 20, canvas.height / 2 - 30);
 }
 
 // Create the canvas
